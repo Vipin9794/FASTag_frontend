8402165026e3705d8f3e3f04f3c680e88bdaaf07
Vipin9794 <rikyverma112@gmail.com>
2025-10-31 17:35:19 +0530

    Convert mobile app using PWA

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,39 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "Fastag Recharge App",
+        short_name: "FastagApp",
+        description: "FASTag recharge web app built with React + Vite",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#1a73e8",
+        icons: [
+          {
+            src: "/icons/iconL-144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+          },
+          {
+            src: "/icons/icon-144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+          },
+        ],
+       
+        
+      },
+    }),
   ],
-
-})
+});
